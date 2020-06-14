5628d621500fa73f5b66ad0c00cb02c0ad1027e4
ET <Earnestwt3@gmail.com>
2020-06-14 12:57:10 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let person = {
+    name: 'ET',
+    age: 30
+};
+// Dot Notation
+person.name = 'ETph';
+
+// Bracket Notation
+person['age'] = '2077'
+
+console.log(person.name);
+console.log(person.age);
